0a2849942d46265ddbbfe3e2c230c94cc29f0886
Ivan Rubinson <soryy708@gmail.com>
2019-09-14 16:30:57 +0300

    Whitespace & comments

--- transform.js
+++ transform.js
@@ -2,16 +2,21 @@ var fs = require('fs');
 const rawData = require('./rawData');
 const solarSystems = rawData.solarSystems;
 const jumps = rawData.jumps;
+
+// We don't do hauling through low & null sec systems, so lets filter them out
 const relevantSolarSystems = solarSystems.filter((solarSystem) => Math.round(solarSystem.security) >= 0.5);
+
 const formattedSystems = {};
 relevantSolarSystems.forEach((solarSystem) => {
     const outboundJumps = jumps.filter((jump) => jump.from === solarSystem.id);
+
     let connectedSystems = outboundJumps.map((jump) => {
         const systemId = jump.to;
         const solarSystem = relevantSolarSystems.find((solarSystem) => solarSystem.id === systemId);
         return solarSystem && solarSystem.name.toLowerCase();
     });
     connectedSystems = connectedSystems.filter((system) => !!system); // remove empty ones (like lowsec)
+    
     formattedSystems[solarSystem.name.toLowerCase()] = connectedSystems;
 });
 
